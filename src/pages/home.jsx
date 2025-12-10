57727bb46cd6af14f79f7fc2396aa60fb5a19041
Francisco <figueroaf@moravian.edu>
2025-12-09 23:59:56 -0500

    added images

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,9 @@
 import './home.css'
 import logo from '../assets/logo.png'
 import hotel from '../assets/hotel-background.jpg'
+import manage from '../assets/manage.png'
+import privacy from '../assets/privacy.png'
+import like from '../assets/like.png'
 
 export const Home = () => {
   return (
@@ -14,10 +17,21 @@ export const Home = () => {
       >
         <div className='home-text-div'>
           <img className='home-img' src={logo}></img>
-          <h1 className='home-title'>Enter your hotel to get started</h1>
+          <h1 className='home-title'>Enter your <strong>hotel</strong> to get started</h1>
           <input className='test-input' type="text" value="" />
+          <p className='caption'>I'd like to look up a hotel by name</p>
+        </div>
+        <h1 className='home-join'>Join the RMH Family</h1>
+        <h1 className='home-join-caption'>Love RMP? Let's make it official.</h1>
+        <div className='image-grid'>
+          <img  src={manage}></img>
+          <img  src={privacy}></img>
+          <img  src={like}></img>
+
+
 
         </div>
+
 
       </div>
 
